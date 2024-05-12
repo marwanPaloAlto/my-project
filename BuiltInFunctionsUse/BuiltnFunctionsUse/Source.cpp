b1f7495e741fa853c4b0f7fb007848d1049ddbf4
Marwan Saeed <marwanbakr449@gmail.com>
2024-05-12 19:05:17 +0300

    Add files via upload

---
+++ BuiltInFunctionsUse/BuiltnFunctionsUse/Source.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+#include <cmath>
+
+int main()
+{
+	int i = -5;
+	double w = 5.0, z = 2.0;
+
+
+
+
+	cout << " The absolute value of i is " << abs(i) << "\n";
+	cout << "the power of x to y is " << pow(w, z) << "\n";
+}
+
