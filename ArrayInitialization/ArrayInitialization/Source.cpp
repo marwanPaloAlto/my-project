567b1bca7d75bb367cd43a2d6ca9d9c0fc4b470b
Marwan Saeed <marwanbakr449@gmail.com>
2024-05-12 18:58:46 +0300

    Add files via upload

---
+++ ArrayInitialization/ArrayInitialization/Source.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	int list[10];
+
+	for (int i = 0; i < 10; i++)
+	{
+
+		//list[i] = 0; áæ ÚÇíÒ ÇØ ÇáŞíã ÈäİÓí
+		cout << " Enter item value \n";
+		cin >> list[i];
+
+
+	}
+	cout << list[5] << "\n";
+}
